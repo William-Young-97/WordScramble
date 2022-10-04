1a5d38cd9d734ed8de8163e7aa34c5664823c24b
William Young <will.young@engineerbetter.com>
2022-10-04 17:37:08 +0100

    Initial commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by William Young on 04/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
